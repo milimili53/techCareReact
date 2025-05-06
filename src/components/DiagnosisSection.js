f7f84f5624089611347c0b948252db92bd996902
Milijan <milijanjic16@gmail.com>
2025-05-06 16:02:25 +0200

    full version

--- src/components/DiagnosisSection.js
+++ src/components/DiagnosisSection.js
@@ -70,97 +70,15 @@ const newData = {
   ]
 };
 
-
-
-/*////////////////////CHART///////////////////////
-
-
-function updateChart(selectedValue) {
-  const dataLength = selectedValue === 'all' ? diagnosisHistory.length : parseInt(selectedValue);
-  
-  const newData = {
-      labels: diagnosisHistory.slice(0, dataLength),
-      datasets: [
-          {
-              label: "Systolic Pressure",
-              data: systolicValues.slice(0, dataLength),
-              borderColor: '#C26EB4',
-              backgroundColor: "rgba(255, 99, 132, 0.2)",
-              borderWidth: 2,
-              pointRadius: 4,
-              pointBackgroundColor: '#E66FD2',
-              pointBorderWidth: 1,
-              pointBorderColor: '#fff',
-              tension: 0.4,
-          },
-          {
-              label: "Diastolic Pressure",
-              data: diastolicValues.slice(0, dataLength),
-              borderColor: "#7E6CAB",
-              backgroundColor: "rgba(54, 162, 235, 0.2)",
-              borderWidth: 2,
-              pointRadius: 4,
-              pointBackgroundColor: '#8C6FE6',
-              pointBorderWidth: 1,
-              pointBorderColor: '#fff',
-              tension: 0.4,
-          }
-      ]
-  };
-
-  if (bloodPressureChart) {
-      bloodPressureChart.destroy();
-  }
-
-  bloodPressureChart = new Chart(ctx, {
-      type: "line",
-      data: newData,
-      options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          scales: {
-              y: {
-                  beginAtZero: false,
-              }
-          },
-          plugins: {
-              legend: {
-                  display: false 
-              }
-          },
-          scales: {
-              x: {
-                  ticks: {
-                      font: {
-                          size: 10, 
-                          weight: 'bold',
-                          color: '#000' 
-                      }
-                  },
-                  grid: {
-                      display: false, 
-                  }
-              },
-              y: {
-                  ticks: {
-                      font: {
-                          size: 10, 
-                          weight: 'bold',
-                          color: '#000'
-                      }
-                  },
-                  grid: {
-                      drawOnChartArea: true, 
-                      lineWidth: 1, 
-                  }
-              }
-          },
-      }
-  });
-}
-
-/*////////////////////////CHART///////////////////////*/
-
+const options = {
+  plugins: {
+    legend: {
+      display: false, 
+    },
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+};
 
 
 
@@ -219,7 +137,7 @@ const check = (message) => {
           </div>
         </div>
         
-        <Line data={newData} id='canvas'/>
+        <Line data={newData} options={options} id='canvas'/>
       </div>
       <div className="infoPart">
         <div className="infoCard systolic">
--- src/contexts/patientRequest.js
+++ src/contexts/patientRequest.js
@@ -4,40 +4,6 @@ const username = "coalition";
 const password = "skills-test";
 const auth = btoa(`${username}:${password}`); 
 
-/*
-const[bloodPressureChart, setBloodPressureChart] = useState(null);
-const [diagnosisHistory, setDiagnosisHistory] = useState([]);
-const [systolicValues, setSystolicValues] = useState([]);
-const [diastolicValues, setDiastolicValues] = useState([]);
-*/
-
-
-/*
-const fetchCall = () => {
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("https://fedskillstest.coalitiontechnologies.workers.dev", {
-                    method: "GET",
-                    headers: {"Authorization": `Basic ${auth}`},
-                });
-                if (!response.ok) {
-                    throw new Error("Network response was not ok");
-                }
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.error("There has been a problem with your fetch operation:", error);
-            }
-        };
-        
-        fetchData();
-    },[]);
-    
-}
-export default fetchCall
-
-*/
 
 const PatientContext = createContext();
 
@@ -74,16 +40,7 @@ export function PatientProvider({ children }) {
     )
     
 }
-/*
-export const findPatient = (name) => {
-    const [patient, setPatient] = useState({});
 
-    const foundPatient = patients.find((patient) => patient.name === name);
-    setPatient(foundPatient);
-
-    return patient;
-};
-*/
 export const usePatients = () => {
     return useContext(PatientContext);
 };
